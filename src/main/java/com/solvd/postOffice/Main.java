3b5f7926af706d34147d2cdea0009f097a09fe30
odariko <daria.gvendoline@gmail.com>
2022-08-10 15:25:49 +0300

    add

--- src/main/java/com/solvd/postOffice/Main.java
+++ src/main/java/com/solvd/postOffice/Main.java
@@ -4,6 +4,6 @@ import org.apache.logging.log4j.Logger;
 public class Main {
     private static final Logger log = LogManager.getLogger();
     public static void main(String[] args) {
-        log.info("Hello this is an info message");
+        log.info( " Hello this is an info message");
     }
 }
